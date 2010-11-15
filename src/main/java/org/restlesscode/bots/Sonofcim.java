8209b1b338633c98516dd34193a7bd8daafb584b
Mike Mattozzi <mmattozzi@abaeterno.local>
2010-11-15 11:33:46 -0500

    Up

--- src/main/java/org/restlesscode/bots/Sonofcim.java
+++ src/main/java/org/restlesscode/bots/Sonofcim.java
@@ -21,7 +21,7 @@ public class Sonofcim extends IrcBot {
         // getMessageHandlerChain().addMessageListener(new LivelockAnswerer());
         getMessageHandlerChain().addMessageListener(new LostContextQuoter(), 0.33f);
         YahooAnswerer yahooAnswerer = new YahooAnswerer();
-        yahooAnswerer.setYahooId("sjaCBTfV34G0r7eTZhywNQ9b8WBrWpk6CY.3G4jZDP3JGeXJHPWfU40rKc4B2p_CLoM7");
+        yahooAnswerer.setYahooId("");
         getMessageHandlerChain().addMessageListener(yahooAnswerer, 0.75f);
         getMessageHandlerChain().addMessageListener(new RandomChannelQuoter());
 
